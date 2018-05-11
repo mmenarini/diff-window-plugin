6d9405d5f3a39619c898ed14148784aca032655d
Sander Valstar <sander@email.com>
2018-05-10 17:47:23 -0700

    adds persistent settings storage to store gettyPath

--- src/main/java/edu/ucsd/AppState.java
+++ src/main/java/edu/ucsd/AppState.java
@@ -1,11 +1,7 @@
 package edu.ucsd;
 
-import com.intellij.openapi.vfs.VirtualFile;
 import io.reactivex.Observable;
 import io.reactivex.subjects.BehaviorSubject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class AppState {
     private static BehaviorSubject<ClassMethod> currentClassMethod = BehaviorSubject.create();
